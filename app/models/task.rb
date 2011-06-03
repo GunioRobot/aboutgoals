5d4b55dc211d300cffc982eb66fb71631b917637
andyr <andy.roberts@jgp.co.uk>
2011-06-03 17:37:31 +0100

    status method for task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :goal
+
+  def status
+    complete ? :fully_complete : :in_progress
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,9 +7,27 @@ describe Task do
       :detail => "value for detail",
       :complete => false
     }
+    @it = Task.create!(@valid_attributes)
   end
 
   it "should create a new instance given valid attributes" do
     Task.create!(@valid_attributes)
   end
+
+  describe :status do
+    context "when task is complete" do
+      before(:each) { @it.complete = true }
+      it "should have status :fully_complete" do
+        @it.status.should == :fully_complete
+      end
+    end
+  
+   context "when task is not complete" do
+      before(:each) { @it.complete = false }
+      it "should have status :in_progress" do
+        @it.status.should == :in_progress
+      end
+    end
+  end
+  
 end
