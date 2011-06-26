551ec45609222ba5ff3722481931f0b3a5ab5ffe
andyr <andy.roberts@jgp.co.uk>
2011-06-26 14:16:59 +0100

    specs for links to new on empty list

--- spec/views/activities/index.html.erb_spec.rb
+++ spec/views/activities/index.html.erb_spec.rb
@@ -3,24 +3,38 @@ require 'spec_helper'
 describe "/activities/index.html.erb" do
   include ActivitiesHelper
 
-  before(:each) do
-    assigns[:activities] = [
-      stub_model(Activity,
-        :name => "value for name",
-        :detail => "value for detail",
-        :complete => false
-      ),
-      stub_model(Activity,
-        :name => "value for another name",
-        :detail => "value for different detail",
-        :complete => true
-      )
-    ]
+  describe "with some activities" do
+    before(:each) do
+      assigns[:activities] = [
+        stub_model(Activity,
+          :name => "value for name",
+          :detail => "value for detail",
+          :complete => false
+        ),
+        stub_model(Activity,
+          :name => "value for another name",
+          :detail => "value for different detail",
+          :complete => true
+        )
+      ]
+    end
+
+    it "renders a list of activities" do
+      render
+      response.should have_text(/value\ for\ name/)
+      response.should have_text(/value\ for\ another\ name/)
+    end
   end
 
-  it "renders a list of activities" do
-    render
-    response.should have_text(/value\ for\ name/)
-    response.should have_text(/value\ for\ another\ name/)
+  describe "with no activities" do
+    before(:each) do
+      assigns[:activities] = []
+    end
+
+    it "links to new" do
+      render
+      response.should have_tag('a[href=?]',new_activity_path)
+    end
   end
+
 end
--- spec/views/goals/summary.html.erb_spec.rb
+++ spec/views/goals/summary.html.erb_spec.rb
@@ -3,14 +3,38 @@ require 'spec_helper'
 describe "/goals/summary.html.erb" do
   include GoalsHelper
 
-  before(:each) do
-    assigns[:goals] = [
-      stub_model(Goal),
-      stub_model(Goal)
-    ]
+  describe "with some goals" do
+    before(:each) do
+      assigns[:goals] = [
+        stub_model(Goal, 
+          :name => "value for name",
+          :detail => "value for detail",
+          :complete => false
+                  ),
+        stub_model(Goal, 
+          :name => "value for another name",
+          :detail => "value for different detail",
+          :complete => true
+                  )
+      ]
+    end
+
+    it "renders a list of goals" do
+      render
+      response.should have_text(/value\ for\ name/)
+      response.should have_text(/value\ for\ another\ name/)
+    end
   end
 
-  it "renders a list of goals" do
-    render
+  describe "with no goals" do
+    before(:each) do
+      assigns[:goals] = []
+    end
+
+    it "links to new" do
+      render
+      response.should have_tag('a[href=?]',new_goal_path)
+    end
   end
+
 end
